0f99f8de233de168ea8c6ae1594332051a30b287
Gunnar Knutson <boldwarriorhs@gmail.com>
2020-08-08 23:10:51 -0700

    create venv and main python cli file.

---
+++ DiceRoll (CLI).py
@@ -0,0 +1,13 @@
+/*
+
+*/
+
+import time
+
+version = 0.0.1
+year = 2020
+
+print("DiceRoll v " + version + " Copyright (C) " + year + " Gunnar Knutson")
+print("This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.")
+print("This is free software, and you are welcome to redistribute it")
+print("under certain conditions; type 'show c' for details.")
